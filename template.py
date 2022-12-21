0f997ca3b9e823cdfe43b5ced1ac3a121a8dc1c0
hmiemad <51701257+hmiemad@users.noreply.github.com>
2022-12-21 22:54:42 +0100

    Update template.py

--- template.py
+++ template.py
@@ -9,6 +9,8 @@ normal_2 = np.random.normal(5,2,20000)
 normal_3 = np.random.normal(-10,2,20000)
 trimodal = np.concatenate([normal_1,normal_2,normal_3])
 
+print(f"Trimodal data, mean = {trimodal.mean():5.1f}, std = {trimodal.std():5.1f}")
+
 fig, ax = plt.subplots(figsize = (15,10))
 
 _, bins, _ = ax.hist(trimodal, 100, density=True)
@@ -18,6 +20,6 @@ for mu_0 in np.linspace(-12,8,21):
     y = w*norm(mu,sigma).pdf(bins)
     ax.plot(bins, y, '--')
 
-    print(f"{mu_0:5.1f} : {w:.2f}.Normal({mu:.1f},{sigma:.1f})")
+    print(f"{mu_0:5.1f} : {w:5.2f} x Normal({mu:5.1f} ,{sigma:4.1f})")
 
 fig.show()
