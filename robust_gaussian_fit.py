c4ad74c26aae44f10590ab66c14a5e357e7cab2a
hmiemad <51701257+hmiemad@users.noreply.github.com>
2022-12-22 16:23:46 +0100

    Update robust_gaussian_fit.py

--- robust_gaussian_fit.py
+++ robust_gaussian_fit.py
@@ -1,10 +1,20 @@
 import numpy as np
-from scipy.stats import norm
 
-#Integral of a normal distribution from -x to x
-truncated_integral = lambda x : 2*norm.cdf(x)-1
-#Standard deviation of a truncated normal distribution from -x to x
-truncated_sigma = lambda x:np.sqrt(1-2*x*norm.pdf(x)/truncated_integral(x))
+def normal_erf(x, depth = 50):
+    ele = 1.0
+    normal = 1.0
+    erf = x
+    for i in range(1,depth):
+        ele = - ele * x * x/2.0/i
+        normal = normal + ele
+        erf = erf + ele * x / (2.0 * i + 1)
+
+    return normal/np.sqrt(2.0*np.pi) , erf/np.sqrt(2.0*np.pi)
+
+def truncated_intergral_and_sigma(x):
+    n,e = normal_erf(x)
+    return 2*e, np.sqrt(1-n*x/e)
+
 
 def robust_gaussian_fit(X, mu = None, sigma = None, bandwidth = 1.0, eps = 1.0e-5, weights = None):
 
@@ -24,6 +34,7 @@ def robust_gaussian_fit(X, mu = None, sigma = None, bandwidth = 1.0, eps = 1.0e-
     """
 
     w,w0=0,2
+    
     if weights is None:
         weights = np.ones(X.shape)
     else :
@@ -37,26 +48,24 @@ def robust_gaussian_fit(X, mu = None, sigma = None, bandwidth = 1.0, eps = 1.0e-
         #rule of thumb
         sigma = np.std(X)/3
         
-    bandwidth_truncated_normal_weight = truncated_integral(bandwidth)
-    bandwidth_truncated_normal_sigma = truncated_sigma(bandwidth)
+    bandwidth_truncated_normal_weight, bandwidth_truncated_normal_sigma = truncated_intergral_and_sigma(bandwidth)
+
     
     while abs(w - w0) > eps:
         #loop until tolerence is reached
-        try:
-            """
-            create a window on X around mu of width 2*bandwidth*sigma
-            find the mean of that window to shift the window to most expected local value
-            measure the standard deviation of the window and divide by the standard deviation of a truncated gaussian distribution
-            measure the proportion of points inside the window, divide by the weight of a truncated gaussian distribution
-            """
-            W = np.where(np.logical_and(X - mu - bandwidth * sigma < 0 , X - mu + bandwidth * sigma > 0), 1, 0)
-            mu = np.average(X[W == 1], weights = weights[W == 1])
-            var = np.average(np.square(X[W == 1]), weights = weights[W == 1]) - mu**2
-            sigma = np.sqrt(var)/bandwidth_truncated_normal_sigma
-            w0 = w
-            w = np.average(W, weights = weights)/bandwidth_truncated_normal_weight
-        
-        except:
-            break
-    
+
+        """
+        create a uniform window on X around mu of width 2*bandwidth*sigma
+        find the mean of that window to shift the window to most expected local value
+        measure the standard deviation of the window and divide by the standard deviation of a truncated gaussian distribution
+        measure the proportion of points inside the window, divide by the weight of a truncated gaussian distribution
+        """
+        W = np.where(np.logical_and(X - mu - bandwidth * sigma < 0 , X - mu + bandwidth * sigma > 0), 1, 0)
+        mu = np.average(X[W == 1], weights = weights[W == 1])
+        var = np.average(np.square(X[W == 1]), weights = weights[W == 1]) - mu**2
+        sigma = np.sqrt(var)/bandwidth_truncated_normal_sigma
+        w0 = w
+        w = np.average(W, weights = weights)/bandwidth_truncated_normal_weight
+
     return w,mu,sigma
+
